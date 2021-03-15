f04f826baf5912482bc39c9bca87a8ef65bdc45a
arover <ryan.arover@gmail.com>
2021-03-16 01:24:18 +0800

    save keypair to files. add load private key function.

--- app/src/main/java/com/arover/logger/MainActivity.java
+++ app/src/main/java/com/arover/logger/MainActivity.java
@@ -2,19 +2,11 @@ package com.arover.logger;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
-import android.os.Environment;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.provider.MediaStore;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,22 +16,39 @@ import com.arover.app.crypto.RsaCipher;
 import com.arover.app.logger.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.annotations.Nullable;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final int PICKFILE_RESULT_CODE = 101;
+    private static final int PICK_FILE_RESULT_CODE = 101;
     private EditText textInput;
     private KeyPair keypair;
-    private byte[] encrypteInputText;
+    private byte[] encryptedInputText;
     private TextView resultTxt;
     private TextView resultTxt2;
+    private Uri logFileUri;
+    private byte[] privateKey;
+
+    private final LinkedList<Disposable> disposables = new LinkedList<>();
 
     @SuppressWarnings("handlerleak")
     Handler handler = new Handler() {
@@ -52,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private Uri logFileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,19 +69,23 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate");
 
         setContentView(R.layout.activity_main);
-        textInput = (EditText) findViewById(R.id.text);
-        resultTxt = (TextView) findViewById(R.id.result);
-        resultTxt2 = (TextView) findViewById(R.id.result2);
+        textInput = findViewById(R.id.text);
+        resultTxt = findViewById(R.id.result);
+        resultTxt2 = findViewById(R.id.result2);
 
         findViewById(R.id.decrypt_file).setOnClickListener(v -> {
             if (logFileUri == null) {
-                Toast.makeText(getApplicationContext(), "please choose log file first!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "please choose log file first!",
+                        Toast.LENGTH_SHORT).show();
             } else {
                 decryptLog(logFileUri);
             }
         });
         findViewById(R.id.choose_file_btn).setOnClickListener(v -> {
             chooseFile();
+        });
+        findViewById(R.id.load_private_key).setOnClickListener(v -> {
+            loadPrivateKey();
         });
 //        findViewById(R.id.decrypt_btn).setOnClickListener(v -> {
 //            decryptTxt();
@@ -97,13 +109,70 @@ public class MainActivity extends AppCompatActivity {
 //        handler.sendEmptyMessageDelayed(0,1000);
     }
 
+    private void loadPrivateKey() {
+        Context appContext = getApplicationContext();
+        autoClean(
+                loadPrivateFile(appContext)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe((byte[] key) -> {
+                            privateKey = key;
+                            android.util.Log.i(TAG,
+                                    "loadPrivateFile key=" + Util.bytesToHexString(privateKey));
+                            Toast.makeText(getApplicationContext(), "load private key success",
+                                    Toast.LENGTH_SHORT).show();
+                        }, e -> Toast.makeText(getApplicationContext(), e.getMessage(),
+                                Toast.LENGTH_SHORT).show()));
+    }
+
+    private Single<byte[]> loadPrivateFile(Context applicationContext) {
+        return Single.create(emitter -> {
+
+            File privateKeyFile = new File(
+                    applicationContext.getExternalFilesDir(null) + "/log_private.key");
+
+            if (!privateKeyFile.exists()) {
+                emitter.onError(new FileNotFoundException("private key file not exist"));
+                return;
+            }
+
+            InputStream in = null;
+            try {
+                in = new FileInputStream(privateKeyFile);
+                byte[] buf = new byte[2048];
+                int len = in.read(buf);
+                if (len <= 1024) {
+                    throw new IllegalStateException("empty private key file?");
+                }
+                byte[] result = new byte[len];
+                System.arraycopy(buf, 0, result, 0, len);
+                emitter.onSuccess(result);
+            } catch (Exception e) {
+                emitter.onError(e);
+            } finally {
+                Util.closeQuietly(in);
+            }
+        });
+    }
+
     private void createKeyPair() {
         try {
             keypair = RsaCipher.createKeyPair();
-            resultTxt.setText(getString(R.string.public_key_is, Util.bytesToHexString(keypair.getPublic().getEncoded())));
-            resultTxt2.setText(getString(R.string.private_key_is, Util.bytesToHexString(keypair.getPrivate().getEncoded())));
+            resultTxt.setText(getString(R.string.public_key_is,
+                    Util.bytesToHexString(keypair.getPublic().getEncoded())));
+            resultTxt2.setText(getString(R.string.private_key_is,
+                    Util.bytesToHexString(keypair.getPrivate().getEncoded())));
             Context appContext = getApplicationContext();
-            new Thread(() -> saveKeypairToFile(appContext, keypair)).start();
+            privateKey = keypair.getPrivate().getEncoded();
+            autoClean(
+                    saveKeypairToFile(appContext, keypair)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(() -> {
+
+                            }, e -> Toast.makeText(getApplicationContext(), e.getMessage(),
+                                    Toast.LENGTH_SHORT).show()));
+
 
             android.util.Log.d(TAG, "public key=" + resultTxt.getText());
             android.util.Log.d(TAG, "private key=" + resultTxt2.getText());
@@ -113,33 +182,61 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void saveKeypairToFile(Context applicationContext, KeyPair keypair) {
-        File file = new File(applicationContext.getExternalFilesDir(null) + "/keypair.txt");
-        try {
-            OutputStream out = new FileOutputStream(file);
-            out.write("public key:\n".getBytes());
-            out.write(Util.bytesToHexString(keypair.getPublic().getEncoded()).getBytes());
-            out.write("\n\n".getBytes());
-            out.write("private key:\n".getBytes());
-            out.write(Util.bytesToHexString(keypair.getPrivate().getEncoded()).getBytes());
-
-            new Handler(Looper.getMainLooper()).post(() -> {
-                Toast.makeText(applicationContext,
-                        "key file save in:"+file.getAbsolutePath(),
-                        Toast.LENGTH_LONG).show();
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void autoClean(Disposable subscribeWith) {
+        synchronized (disposables) {
+            cleanDisposed();
+            disposables.add(subscribeWith);
         }
+    }
+
+    private void cleanDisposed() {
+        Iterator<Disposable> it;
+        for (it = disposables.iterator(); it.hasNext(); ) {
+            if (it.next().isDisposed()) {
+                it.remove();
+            }
+        }
+    }
+
+
+    private Completable saveKeypairToFile(Context applicationContext, KeyPair keypair) {
+        return Completable.create(emitter -> {
+            File privateKeyFile = new File(applicationContext.getExternalFilesDir(null) +
+                    "/log_private.key");
+            File publicKeyFile = new File(applicationContext.getExternalFilesDir(null)
+                    + "/log_public.key");
+            OutputStream publicFileOut = null, privateFileOut = null;
+            try {
+                publicFileOut = new FileOutputStream(publicKeyFile);
+                publicFileOut.write(keypair.getPublic().getEncoded());
+                publicFileOut.flush();
+
+                privateFileOut = new FileOutputStream(privateKeyFile);
+                privateFileOut.write(keypair.getPrivate().getEncoded());
+                privateFileOut.flush();
+
+                new Handler(Looper.getMainLooper()).post(() -> {
+                    Toast.makeText(applicationContext,
+                            "key file saved ",
+                            Toast.LENGTH_LONG).show();
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                Util.closeQuietly(publicFileOut);
+                Util.closeQuietly(privateFileOut);
+            }
+        });
     }
 
     private void encryptTxt() {
         String input = textInput.getText().toString();
         android.util.Log.d(TAG, "encryptTxt=" + input);
         try {
-            encrypteInputText = RsaCipher.encrypt(input.getBytes(), keypair.getPublic().getEncoded());
-            resultTxt.setText(Util.bytesToHexString(encrypteInputText));
-            android.util.Log.d(TAG, "encryptTxt len=" + encrypteInputText.length);
+            encryptedInputText = RsaCipher
+                    .encrypt(input.getBytes(), keypair.getPublic().getEncoded());
+            resultTxt.setText(Util.bytesToHexString(encryptedInputText));
+            android.util.Log.d(TAG, "encryptTxt len=" + encryptedInputText.length);
         } catch (Exception e) {
             android.util.Log.d(TAG, "encryptTxt=" + input, e);
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -149,7 +246,8 @@ public class MainActivity extends AppCompatActivity {
     private void decryptTxt() {
 
         try {
-            byte[] decryptBytes = RsaCipher.decrypt(encrypteInputText, keypair.getPrivate().getEncoded());
+            byte[] decryptBytes = RsaCipher
+                    .decrypt(encryptedInputText, keypair.getPrivate().getEncoded());
             resultTxt.setText(new String(decryptBytes));
         } catch (Exception e) {
             android.util.Log.e(TAG, "decryptTxt=", e);
@@ -161,7 +259,7 @@ public class MainActivity extends AppCompatActivity {
         Context appContext = getApplicationContext();
         String file = fileUri.getPath();
 
-        if(file != null && !file.endsWith(".log")){
+        if (file != null && !file.endsWith(".log")) {
             Toast.makeText(getApplicationContext(), "invalid file, choose .log file!",
                     Toast.LENGTH_LONG).show();
             return;
@@ -169,16 +267,18 @@ public class MainActivity extends AppCompatActivity {
 
         new Thread(() -> {
             android.util.Log.d(TAG, "decryptLog " + file);
-            byte[] privateKey = Util.hexStringToBytes(((App) getApplication()).getPrivateKey());
+
             try {
                 Log.decryptLogFile(appContext, fileUri, privateKey);
                 new Handler(Looper.getMainLooper()).post(() -> {
-                    Toast.makeText(appContext, "decrypt log file success", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(appContext, "decrypt log file success", Toast.LENGTH_SHORT)
+                            .show();
                 });
             } catch (Exception e) {
                 e.printStackTrace();
                 new Handler(Looper.getMainLooper()).post(() -> {
-                    Toast.makeText(appContext, "decrypt log file error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(appContext, "decrypt log file error:" + e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
                 });
             }
         }).start();
@@ -200,6 +300,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.flush();
+        synchronized (disposables) {
+            cleanDisposed();
+        }
     }
 
     @Override
@@ -207,17 +310,29 @@ public class MainActivity extends AppCompatActivity {
         handler.removeCallbacksAndMessages(null);
         handler = null;
         Log.flush();
+
+        synchronized (disposables) {
+            for (Disposable it : disposables) {
+                it.dispose();
+            }
+            disposables.clear();
+        }
         super.onDestroy();
     }
 
 
     public void chooseFile() {
+        if(privateKey == null){
+            Toast.makeText(getApplicationContext(), "please load private key first",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
         Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
         chooseFile.addCategory(Intent.CATEGORY_OPENABLE);
         chooseFile.setType("application/*");
         startActivityForResult(
                 Intent.createChooser(chooseFile, getString(R.string.choose_log_file_title)),
-                PICKFILE_RESULT_CODE
+                PICK_FILE_RESULT_CODE
         );
     }
 
